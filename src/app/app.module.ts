fded14516a4270bce26255409a1e3987933fadd0
hamhoum10 <omriyasser12@gmail.com>
2023-10-05 17:53:26 +0100

    workshop2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListUserComponentComponent } from './list-user-component./list-user-component..component';
+import { ListUserComponent } from './list-user/list-user.component';
+import { FooterComponent } from './footer/footer.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { AccountListComponent } from './account-list/account-list.component';
+import { SlideComponent } from './slide/slide.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    ListUserComponentComponent,
+    ListUserComponent,
+    FooterComponent,
+    NavbarComponent,
+    AccountListComponent,
+    SlideComponent,
   ],
   imports: [
     BrowserModule,
